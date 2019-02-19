71eb77e8738381781e345cd56ebf8b0aebe9a175
DoaaAlsafadi <d.alsafadi.jo@gmail>
2019-02-19 21:13:41 +0200

    home

--- educationalWeb/src/app/app.module.ts
+++ educationalWeb/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeModule
   ],
   imports: [
     BrowserModule
