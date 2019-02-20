e090bc8c68df5b0ef056ef7f7065ff7ccf3ee150
Doa'a Al-Safadi <Doa'a.Safadi@dot.market>
2019-02-20 10:57:50 +0200

    header & footer

--- educationalWeb/src/app/app.module.ts
+++ educationalWeb/src/app/app.module.ts
@@ -4,8 +4,10 @@ import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, HomeModule, RouterModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
